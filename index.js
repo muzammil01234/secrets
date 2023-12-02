3c3b2fd91571edda6ff5809aa3fe3da417d1887c
Muzammil0123 <21je0862@iitism.ac.in>
2023-12-02 09:59:41 +0530

    I have finished my basic work on creating secret project

---
+++ index.js
@@ -0,0 +1,29 @@
+// HINTS:
+// 1. Import express and axios
+
+// 2. Create an express app and set the port number.
+
+// 3. Use the public folder for static files.
+
+// 4. When the user goes to the home page it should render the index.ejs file.
+
+// 5. Use axios to get a random secret and pass it to index.ejs to display the
+// secret and the username of the secret.
+
+// 6. Listen on your predefined port and start the server.
+import express from "express";
+import axios from "axios";
+const app=express();
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+     axios.get('https://secrets-api.appbrewery.com/random').then((response)=>{
+       let dat=response.data;
+       res.render('index.ejs',{result:dat});
+    })
+    
+})
+app.listen(3000,()=>{
+    console.log("Your server is running on port 3000");
+})
